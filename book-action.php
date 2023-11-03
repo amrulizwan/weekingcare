631ded5cd29ee73629e0f582d245da98c575feec
Amrul Izwan <amrulizwan7@gmail.com>
2023-11-04 05:56:23 +0800

    tambah be-booking sitik

---
+++ book-action.php
@@ -0,0 +1,10 @@
+<?php 
+isset($_POST['submit']) ? $submit = $_POST['submit'] : $submit = '';
+$nama = $_POST['name'];
+$email = $_POST['email'];
+$data1 = $_POST['data1'];
+$data2 = $_POST['data2'];
+$message = $_POST['message'];
+
+header("Location:https://api.whatsapp.com/send?phone=6285238543351&text=Halo%20saya%20".$nama."%0ASaya%20ingin%20".$data1."%0ADan%20Ingin".$data2."0A".$message."");
+?>
